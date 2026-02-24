1a5f224db4e24b9727cbb9aea049c1211b6a1600
Mahaldi WIjaya Saputra <aldiwijayasaputra@gmail.com>
2026-02-24 21:25:50 +0700

    fix wrong curly braches, add recent scores section

--- HPTrivia/Views/ContentView.swift
+++ HPTrivia/Views/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
     @State private var showInstructions = false
     @State private var showSettings = false
     @State private var playGame = false
-     
+    
     var body: some View {
         GeometryReader { geo in
             ZStack {
@@ -48,74 +48,88 @@ struct ContentView: View {
                             .padding(.top, 70)
                             .transition(.move(edge: .top))
                         }
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        HStack {
-                            Spacer()
-                            VStack {
-                                if animateViewIn {
-                                    Button {
-                                        showInstructions.toggle()
-                                    } label: {
-                                        Image(systemName: "info.circle.fill")
-                                            .font(.largeTitle)
-                                            .foregroundStyle(.white)
-                                            .shadow(radius: 5)
-                                    }
-                                    .transition(.offset(x: -geo.size.width / 4))
-                                }
-                            }
-                            .animation(.easeOut(duration: 0.7).delay(2.7), value: animateViewIn)
-                            Spacer()
-                            VStack {
-                                if animateViewIn {
-                                    Button {
-                                        playGame.toggle()
-                                    } label: {
-                                        Text("Play ")
-                                            .font(.largeTitle)
-                                            .foregroundStyle(.white)
-                                            .padding(.vertical, 7)
-                                            .padding(.horizontal, 50)
-                                            .background(.brown)
-                                            .clipShape(.rect(cornerRadius: 7))
-                                            .shadow(radius: 5)
-                                            .scaleEffect(scalePlayBtn ? 1.2 : 1)
-                                            .onAppear {
-                                                withAnimation(.easeInOut(duration: 1.3).repeatForever()) {
-                                                    scalePlayBtn = true
-                                                }
-                                            }
-                                    }
-                                    .transition(.offset(y: geo.size.height / 3))
-                                }
-                            }
-                            .animation(.easeOut(duration: 0.7).delay(2), value: animateViewIn)
-                            Spacer()
-                            VStack {
-                                if animateViewIn {
-                                    Button {
-                                        showSettings.toggle()
-                                    } label: {
-                                        Image(systemName: "gearshape.fill")
-                                            .font(.largeTitle)
-                                            .foregroundStyle(.white)
-                                            .shadow(radius: 5)
-                                    }
-                                    .transition(.offset(x: geo.size.width / 4))
-                                }
-                            }
-                            .animation(.easeOut(duration: 0.7).delay(2.7), value: animateViewIn)
-                            Spacer()
-                        }
-                        .frame(width: geo.size.width)
-                        Spacer()
                     }
                     .animation(.easeOut(duration: 0.7).delay(2), value: animateViewIn)
+                    Spacer()
+                    VStack {
+                        if animateViewIn {
+                            VStack {
+                                Text("Recent Scores")
+                                    .font(.title2)
+                                
+                                Text("33")
+                            }
+                            .font(.title3)
+                            .foregroundStyle(.white)
+                            .padding(.horizontal)
+                            .background(.black.opacity(0.7))
+                            .clipShape(.rect(cornerRadius: 15))
+                            .transition(.opacity)
+                        }
+                    }
+                    .animation(.linear(duration: 1).delay(4), value: animateViewIn)
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        VStack {
+                            if animateViewIn {
+                                Button {
+                                    showInstructions.toggle()
+                                } label: {
+                                    Image(systemName: "info.circle.fill")
+                                        .font(.largeTitle)
+                                        .foregroundStyle(.white)
+                                        .shadow(radius: 5)
+                                }
+                                .transition(.offset(x: -geo.size.width / 4))
+                            }
+                        }
+                        .animation(.easeOut(duration: 0.7).delay(2.7), value: animateViewIn)
+                        Spacer()
+                        VStack {
+                            if animateViewIn {
+                                Button {
+                                    playGame.toggle()
+                                } label: {
+                                    Text("Play ")
+                                        .font(.largeTitle)
+                                        .foregroundStyle(.white)
+                                        .padding(.vertical, 7)
+                                        .padding(.horizontal, 50)
+                                        .background(.brown)
+                                        .clipShape(.rect(cornerRadius: 7))
+                                        .shadow(radius: 5)
+                                        .scaleEffect(scalePlayBtn ? 1.2 : 1)
+                                        .onAppear {
+                                            withAnimation(.easeInOut(duration: 1.3).repeatForever()) {
+                                                scalePlayBtn = true
+                                            }
+                                        }
+                                }
+                                .transition(.offset(y: geo.size.height / 3))
+                            }
+                        }
+                        .animation(.easeOut(duration: 0.7).delay(2), value: animateViewIn)
+                        Spacer()
+                        VStack {
+                            if animateViewIn {
+                                Button {
+                                    showSettings.toggle()
+                                } label: {
+                                    Image(systemName: "gearshape.fill")
+                                        .font(.largeTitle)
+                                        .foregroundStyle(.white)
+                                        .shadow(radius: 5)
+                                }
+                                .transition(.offset(x: geo.size.width / 4))
+                            }
+                        }
+                        .animation(.easeOut(duration: 0.7).delay(2.7), value: animateViewIn)
+                        Spacer()
+                    }
+                    .frame(width: geo.size.width)
+                    Spacer()
                 }
-
-
             }
             .frame(width: geo.size.width, height: geo.size.height)
             
@@ -123,7 +137,7 @@ struct ContentView: View {
         .ignoresSafeArea()
         .onAppear {
             animateViewIn = true
-//            playAudio()
+            //            playAudio()
         }
         .sheet(isPresented: $showInstructions) {
             Instructions()
@@ -140,4 +154,5 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
