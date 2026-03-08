22d46e47e3e3cc2779f8be26d063358f5e4299ea
Mahaldi WIjaya Saputra <aldiwijayasaputra@gmail.com>
2026-03-08 07:59:09 +0700

    add new question and correct function

--- HPTrivia/ViewModels/Game.swift
+++ HPTrivia/ViewModels/Game.swift
@@ -18,7 +18,7 @@ class Game {
     var activeQuestions: [Question] = []
     var answeredQuestions: [Int] = []
     var currentQuestion = try! JSONDecoder().decode([Question].self, from: Data(contentsOf: Bundle.main.url(forResource: "trivia", withExtension: "json")!))[0]
-    var answer: [String] = []
+    var answers: [String] = [] // list options
     
     func startGame() {
         for book in bookQuestions.books {
@@ -31,11 +31,35 @@ class Game {
     }
     
     func newQuestion() {
+        // cek kalo misal semua question udah di jawab, klo udh dijawab semua maka bakal ngulang lagi
+        if answeredQuestions.count == activeQuestions.count {
+            answeredQuestions = []
+        }
         
+        // ambil 1 questiond dari list question yang active
+        currentQuestion = activeQuestions.randomElement()!
+        
+        // selama current question yang di acak itu sama dengan question yang udh di jawab , maka acak lagi current questionnya
+        while(answeredQuestions.contains(currentQuestion.id)) {
+            currentQuestion = activeQuestions.randomElement()!
+        }
+        
+        answers = []
+        answers.append(currentQuestion.answer)
+        
+        for answer in currentQuestion.wrong {
+            answers.append(answer)
+        }
+        
+        answers.shuffle()
+        
+        questionScore = 5
     }
     
     func correct() {
+        answeredQuestions.append(currentQuestion.id)
         
+        gameScore += questionScore
     }
     
     func endGame() {
